b377965a879b2cb4150b11fea86178e646f99612
Appu <techtaletamil@gmail.com>
2026-02-02 09:13:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,22 @@
 const container = document.querySelector(".container");
 const finalImage = document.querySelector(".final-image");
+const baseImage = document.querySelector(".base-image");
 
 function reveal(x, y) {
   finalImage.style.clipPath = `circle(120px at ${x}px ${y}px)`;
+
+  // NEW: move scribble mask
+  baseImage.style.setProperty("--x", `${x}px`);
+  baseImage.style.setProperty("--y", `${y}px`);
 }
 
 function hide() {
   finalImage.style.clipPath = "circle(0px at 50% 50%)";
 }
 
-/* Desktop */
 container.addEventListener("mousemove", (e) => {
   const rect = container.getBoundingClientRect();
   reveal(e.clientX - rect.left, e.clientY - rect.top);
 });
 
-container.addEventListener("mouseleave", hide);
-
-/* Mobile */
-container.addEventListener("touchmove", (e) => {
-  e.preventDefault();
-  const rect = container.getBoundingClientRect();
-  const t = e.touches[0];
-  reveal(t.clientX - rect.left, t.clientY - rect.top);
-}, { passive: false });
-
-container.addEventListener("touchend", hide);
+container.addEventListener("mouseleave", hide); 
